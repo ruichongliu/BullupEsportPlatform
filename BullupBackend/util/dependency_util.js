fc086ccdf62170c01efaa593313bdc1645c2703f
guojingming <1043840980@qq.com>
2017-10-26 10:51:20 +0800

    first commit

---
+++ BullupBackend/util/dependency_util.js
@@ -0,0 +1,50 @@
+var pathUtil = require("./util/path_util.js");
+
+exports.init = function(){
+    pathUtil.init();
+    exports.global = {};
+    exports.global.service = {};
+    exports.global.utils = {};
+    exports.global.listener = {};
+    exports.global.dao = {};
+    
+    exports.global.dao.baseInfoDao = require(pathUtil.paths.daoPaths.baseInfoDaoPath);
+    exports.global.dao.wealthInfoDao = require(pathUtil.paths.daoPaths.wealthInfoDaoPath);
+    exports.global.dao.rankInfoDao = require(pathUtil.paths.daoPaths.rankInfoDaoPath);
+    exports.global.dao.battleRecordDao = require(pathUtil.paths.daoPaths.battleRecordDaoPath);
+    exports.global.dao.lolInfoDao = require(pathUtil.paths.daoPaths.lolInfoDaoPath);
+
+    exports.global.service.userService = require(pathUtil.paths.servicePaths.userServicePath);
+    exports.global.service.battleService = require(pathUtil.paths.servicePaths.battleServicePath);
+    exports.global.service.chatService = require(pathUtil.paths.servicePaths.chatServicePath);
+    exports.global.service.administratorService = require(pathUtil.paths.servicePaths.administratorServicePath);
+    exports.global.service.competitonService = require(pathUtil.paths.servicePaths.competitonServicePath);
+    exports.global.service.feedbackService = require(pathUtil.paths.servicePaths.feedbackServicePath);
+    exports.global.service.lolKeyService = require(pathUtil.paths.servicePaths.lolKeyServicePath);
+    exports.global.service.matchService = require(pathUtil.paths.servicePaths.matchServicePath);
+    exports.global.service.paymentService = require(pathUtil.paths.servicePaths.paymentServicePath);
+    exports.global.service.socketService = require(pathUtil.paths.servicePaths.socketServicePath);
+    exports.global.service.stripeService = require(pathUtil.paths.servicePaths.stripeServicePath);
+    exports.global.service.teamService = require(pathUtil.paths.servicePaths.teamServicePath);
+
+    exports.global.utils.databaseUtil = require(pathUtil.paths.servicePaths.databaseUtilPath);
+    exports.global.utils.pathUtil = pathUtil;
+    exports.global.utils.dependencyUtil = require(pathUtil.paths.servicePaths.dependencyUtilPath);
+    exports.global.utils.logUtil = require(pathUtil.paths.servicePaths.logUtilPath);
+    exports.global.utils.lolUtil = require(pathUtil.paths.servicePaths.lolUtilPath);
+
+    exports.global.listener.connectionListener = require(pathUtil.paths.listenerPaths.connectionListenerPath);
+    exports.global.listener.userListener = require(pathUtil.paths.listenerPaths.userListenerPath);
+    exports.global.listener.battleListener = require(pathUtil.paths.listenerPaths.battleListenerPath);
+    exports.global.listener.chatListener = require(pathUtil.paths.listenerPaths.chatListenerPath);
+    exports.global.listener.administratorListener = require(pathUtil.paths.listenerPaths.administratorListenerPath);
+    exports.global.listener.competitionListener = require(pathUtil.paths.listenerPaths.competitionListenerPath);
+    exports.global.listener.feedbackListener = require(pathUtil.paths.listenerPaths.feedbackListenerPath);
+    exports.global.listener.lolkeyListener = require(pathUtil.paths.listenerPaths.lolKeyListenerPath);
+    exports.global.listener.matchListener = require(pathUtil.paths.listenerPaths.matchListenerPath);
+    exports.global.listener.paymentListener = require(pathUtil.paths.listenerPaths.paymentListenerPath);
+    exports.global.listener.socketListener = require(pathUtil.paths.listenerPaths.socketListenerPath);
+    exports.global.listener.stripeListener = require(pathUtil.paths.listenerPaths.stripeListenerPath);
+    exports.global.listener.teamListener = require(pathUtil.paths.listenerPaths.teamListenerPath);
+
+}
---
+++ BullupBackend/util/path_util.js
@@ -0,0 +1,70 @@
+exports.init = function(){
+    //文件目录路径对象
+    exports.directories = {};
+    //存放数据库相关操作
+    exports.directories.daoDirectory = "./dao/";
+    //存放一些独立的txt文件等
+    exports.directories.otherDirectory = "./other/";
+    //存放监听器层的文件
+    exports.directories.listenserDirectory = "./listenser/";
+    //存放服务层的文件
+    exports.directories.serviceDirectory = "./service/";
+    //存放工具类的文件
+    exports.directories.utilDirectory = "./util/";
+    //根目录
+    exports.directories.rootDirectory = "./";
+
+    //文件路径对象
+    exports.paths = {};
+    exports.paths.daoPaths = {};
+    exports.paths.servicePaths = {};
+    exports.paths.utilPaths = {};
+    exports.paths.listenerPaths = {};
+    exports.paths.utilPaths = {};
+    exports.otherPaths = {};
+    //Dao路径
+    exports.paths.daoPaths.baseInfoDaoPath = exports.directories.daoDirectory + "base_info_dao.js";
+    exports.paths.daoPaths.wealthInfoDaoPath = exports.directories.daoDirectory + "wealth_info_dao.js";
+    exports.paths.daoPaths.rankInfoDaoPath = exports.directories.daoDirectory + "rank_info_dao.js";
+    exports.paths.daoPaths.battleRecordDaoPath = exports.directories.daoDirectory + "battle_record_dao.js";
+    exports.paths.daoPaths.lolInfoDaoPath = exports.directories.daoDirectory + "lol_info_dao.js";
+    //Service路径
+    exports.paths.servicePaths.userServicePath = exports.directories.serviceDirectory + "user_service.js";
+    exports.paths.servicePaths.battleServicePath = exports.directories.serviceDirectory + "battle_service.js";
+    exports.paths.servicePaths.chatServicePath = exports.directories.serviceDirectory + "chat_service.js";
+    exports.paths.servicePaths.administratorServicePath = exports.directories.serviceDirectory + "administrator_service.js";
+    exports.paths.servicePaths.competitonServicePath = exports.directories.serviceDirectory + "competiton_service.js";
+    exports.paths.servicePaths.feedbackServicePath = exports.directories.serviceDirectory + "feedback_service.js";
+    exports.paths.servicePaths.lolKeyServicePath = exports.directories.serviceDirectory + "lolKey_service.js";
+    exports.paths.servicePaths.matchServicePath = exports.directories.serviceDirectory + "match_service.js";
+    exports.paths.servicePaths.paymentServicePath = exports.directories.serviceDirectory + "payment_service.js";
+    exports.paths.servicePaths.socketServicePath = exports.directories.serviceDirectory + "socket_service.js";
+    exports.paths.servicePaths.stripeServicePath = exports.directories.serviceDirectory + "stripe_service.js";
+    exports.paths.servicePaths.teamServicePath = exports.directories.serviceDirectory + "team_service.js";
+    //Util路径
+    exports.paths.utilPaths.databaseUtilPath = exports.directories.utilDirectory + "database_util.js";
+    exports.paths.utilPaths.pathUtilPath = exports.directories.utilDirectory + "path_util.js";
+    exports.paths.utilPaths.dependencyUtilPath = exports.directories.utilDirectory + "dependency_util.js";
+    exports.paths.utilPaths.logUtilPath = exports.directories.utilDirectory + "log_util.js";
+    exports.paths.utilPaths.lolUtilPath = exports.directories.utilDirectory + "lol_util.js";
+    //listener路径
+    //主监听器
+    exports.paths.listenerPaths.connectionListenerPath = exports.directories.listenerDirectory + "connection_listener.js";
+    //具体服务的监听器
+    exports.paths.listenerPaths.userListenerPath = exports.directories.listenserDirectory + "user_listener.js";
+    exports.paths.listenerPaths.battleListenerPath = exports.directories.listenserDirectory + "battle_listener.js";
+    exports.paths.listenerPaths.chatListenerPath = exports.directories.listenserDirectory + "chat_listener.js";
+    exports.paths.listenerPaths.administratorListenerPath = exports.directories.listenserDirectory + "administrator_listener.js";
+    exports.paths.listenerPaths.competitionListenerPath = exports.directories.listenserDirectory + "competition_listener.js";
+    exports.paths.listenerPaths.feedbackListenerPath = exports.directories.listenserDirectory + "feedback_listener.js";
+    exports.paths.listenerPaths.lolKeyListenerPath = exports.directories.listenserDirectory + "lolkey_listener.js";
+    exports.paths.listenerPaths.matchListenerPath = exports.directories.listenserDirectory + "match_listener.js";
+    exports.paths.listenerPaths.paymentListenerPath = exports.directories.listenserDirectory + "payment_listener.js";
+    exports.paths.listenerPaths.socketListenerPath = exports.directories.listenserDirectory + "socket_listener.js";
+    exports.paths.listenerPaths.stripeListenerPath = exports.directories.listenserDirectory + "stripe_listener.js";
+    exports.paths.listenerPaths.teamListenerPath = exports.directories.listenserDirectory + "team_listener.js";
+    //other files路径
+    //存放一些杂文件
+    exports.paths.otherPaths.lolKeyTextPath = exports.directories.otherDirectory + "key.txt";
+
+}
