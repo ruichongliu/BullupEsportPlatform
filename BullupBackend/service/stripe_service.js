e59d003513770e8c4fbb64eb7c65ed726d6cd440
guojingming <1043840980@qq.com>
2017-11-16 06:41:37 +0000

    cxj

--- BullupBackend/service/stripe_service.js
+++ BullupBackend/service/stripe_service.js
@@ -17,9 +17,9 @@ exports.recharge = function(){
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:false}));
     
-    var path1 = 'C:/Users/Administrator/Desktop/BullupEsportPlatform/BullupBackend/other/';
+    var path1 = 'C:/Users/Administrator/Desktop/zuixin/BullupEsportPlatform/BullupBackend/other/';
 
-    var path2 = 'F:/NodeWorkspace/BullupEsport/BullupBackend/other/';
+    var path2 = 'C:/Users/Administrator/Desktop/zuixin/BullupEsportPlatform/BullupBackend/other/';
     app.post('/',function(req,res){
         //var str = req.url.substr(req.url.indexOf('?'), req.url.indexOf('=') - req.url.indexOf('?'));
         var rechargeValue = req.body.rechargeAccount;
