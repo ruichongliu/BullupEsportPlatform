f23133c34ae2a5e33b695f986d1544764424f166
lingniker <lingyuanping@outlook.com>
2017-11-28 18:22:08 +0800

    lingyuanping

--- BullupFrontend/js/playly.js
+++ BullupFrontend/js/playly.js
@@ -5,12 +5,6 @@ $().ready(function(){
         bullup.loadTemplateIntoTarget('swig_team.html', {}, 'main-view');
 		
         });
-   
-
-			document.getElementById('headle_music1').addEventListener('click',function(event){
-				console.log("click ok");            
-				document.getElementById("bullup_background_music").pause();
-			});
     });
    
 
