50392ccc3084e9cc637762d2d362539d496296af
guojingming <1043840980@qq.com>
2017-12-08 16:43:14 +0800

    fix recharge

--- BullupFrontend/js/recharge.js
+++ BullupFrontend/js/recharge.js
@@ -11,15 +11,15 @@ $(document).ready(function(){
         }else if(value < 1){
             bullup.alert("最低充值金额为$1");
         }else{
-            request.post('http://18.221.98.48:3001', {form:{rechargeAccount: value, userId: userInfo.userId}}, function(error, response, body){
+            request.post('http://192.168.2.102:3001', {form:{rechargeAccount: value, userId: userInfo.userId}}, function(error, response, body){
                 if(body == undefined){
                     bullup.alert('订单生产失败，请联系客服！');
                     return;
                 }
-                var bodyStartIndex = body.indexOf("<body>");
-                var bodyEndIndex = body.indexOf("</body>");
-                var htmlStr = body.substr(0, bodyEndIndex);
-                htmlStr = htmlStr.substr(bodyStartIndex + 6, htmlStr.length - 6);
+                var fs = require('fs');
+                fs.writeFileSync('C:/Users/Public/Bullup/temp.html', body);
+               
+                var htmlStr = '<iframe frameborder="0" width="500px" height="800px" src="C:/Users/Public/Bullup/temp.html"></iframe>';
                 $('#main-view').html(htmlStr);
                 $('#recharge').modal('close');
             });
