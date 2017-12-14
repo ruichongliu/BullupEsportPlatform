962e90566b2564303e6d04f0d17365a7a91999ab
LiamShao <liamshao1999@gmail.com>
2017-12-14 18:29:46 +0800

    fix

--- BullupFrontend/js/client.js
+++ BullupFrontend/js/client.js
@@ -1,6 +1,6 @@
 var io = require('socket.io-client');
 
-var socket = io.connect('http://192.168.2.127:3000');
+var socket = io.connect('http://49.140.81.199:3000');
 //var auto_script = require('./js/auto_program/lol_auto_script');
 var lol_process = require('./js/auto_program/lol_process.js');
 var lolUtil = require('./js/util/lol_util.js');
