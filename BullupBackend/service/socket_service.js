b350a0f0b39c9f9d8b146a5e98808d18ea801686
JM.Guo <1043840980@qq.com>
2018-01-01 18:29:58 +0800

    latest

--- BullupBackend/service/socket_service.js
+++ BullupBackend/service/socket_service.js
@@ -11,9 +11,12 @@ exports.init = function() {
     this.roomSocketMap = {};
 
     //用于设置最大重发次数的阈值
-    this.maxResendTimes = 1;///////////////////////////
+    this.maxResendTimes = 10;///////////////////////////
 
-    //多少ms重发一次
+    //延迟多少 超过这个时间才会重发
+    this.timeDelay = 2000;
+
+    //多少ms发送一次
     this.timeInterval = 300;
 
     //用于存储需要发送到客户端的消息
@@ -111,6 +114,9 @@ exports.stableSocketEmit = function(socket, head, data){
     }   
     var token = Math.random().toString(36).substring(7) + socket.id; 
     data.token = token;
+    ////
+    data.lastSendTime = 0;
+    ///
     if(exports.socketEmitQueue[index] != undefined){
         exports.socketEmitQueue[index].dataQueue[String(token)] = {
             'header': head,
@@ -193,6 +199,14 @@ exports.stableEmit = function(){
                 break;
             }
             if(data.blank != true){
+                if(data.lastSendTime == 0){
+                    data.lastSendTime = systemTime;  
+                }else{
+                    if(systemTime - data.lastSendTime <= exports.timeDelay){
+                        continue;
+                    }
+                }
+                
                 socketObj.emit(data.header, data.data); 
                 console.log("tijiao");               
                 delete data;
